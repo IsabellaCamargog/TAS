12e5519de3e6faf5856ca32265184631809af17f
isabellacamargog <isabellacarolinasc@hotmail.com>
2020-08-26 22:30:04 -0300

    Aula 26/08/2020

--- tas-core/src/main/java/br/edu/materdei/tas/core/entity/GrupoEntity.java
+++ tas-core/src/main/java/br/edu/materdei/tas/core/entity/GrupoEntity.java
@@ -2,6 +2,7 @@
 package br.edu.materdei.tas.core.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,7 +12,6 @@ import javax.persistence.Table;
 public class GrupoEntity {
     @Id
     @GeneratedValue
-    
     private Integer id;
     @Column(length = 30, nullable = false)
     private String nome;
